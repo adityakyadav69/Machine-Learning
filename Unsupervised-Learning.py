a35580bd35b6a13f190bfe4de8db1be342810b06
Aditya Yadav <adityakyadav69@gmail.com>
2026-03-11 19:31:37 +0530

    Unsupervised-Learning.py

---
+++ Unsupervised-Learning.py
@@ -0,0 +1,14 @@
+from sklearn.cluster import KMeans
+import numpy as np
+
+# Data
+X = np.array([[1,2], [1,4], [1,0], [10,2], [10,4], [10,0]])
+
+# Model
+kmeans = KMeans(n_clusters=2)
+
+# Train model
+kmeans.fit(X)
+
+# Predict clusters
+print(kmeans.labels_)
